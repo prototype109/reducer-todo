49f7a9deae8692c1c3a9d39f272d218fad24194f
Brandon Nichols <brandonaronnichols@gamil.com>
2019-09-19 23:49:10 -0500

    added reducer function with default case and initial state in reducers folder

---
+++ reducer-todo/src/reducers/reducer.js
@@ -0,0 +1,16 @@
+export const initialState = {
+    list: [
+        {
+            item: 'Learn about reducers',
+            completed: false,
+            id: 3892987589
+        }
+    ]
+}
+
+export const reducer = (state, action) => {
+    switch(action.type){
+        case default:
+            return state;
+    }
+} 
